2dd014c06359d9b68bed45585caf8ce47155ced6
igoryndezp <igoryndezp@gmail.com>
2022-09-17 23:09:52 +0300

    displaying articles on the main page

--- testproject/blog/views.py
+++ testproject/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 
 def home(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    posts = Article.objects.all()
+    res = '<h1>Список статей</h1>'
+    for post in posts:
+        res += f'<div><h3>{post.title}</h3><div>{post.content}</div><div>{post.photo}</div></div><hr>'
+    return HttpResponse(res)
 
 def test(request):
     return HttpResponse('<h1>Test Page</h1>')
