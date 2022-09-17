fe263f62151718dfa256ab8ba2f3e0f825cbb617
igoryndezp <igoryndezp@gmail.com>
2022-09-17 23:36:59 +0300

    home page upgrade

--- testproject/blog/views.py
+++ testproject/blog/views.py
@@ -5,10 +5,8 @@ from .models import Article
 
 def home(request):
     posts = Article.objects.all()
-    res = '<h1>Список статей</h1>'
-    for post in posts:
-        res += f'<div><h3>{post.title}</h3><div>{post.content}</div><div>{post.photo}</div></div><hr>'
-    return HttpResponse(res)
+    return render(request,'blog/home.html',{'posts':posts})
+
 
 def test(request):
     return HttpResponse('<h1>Test Page</h1>')
