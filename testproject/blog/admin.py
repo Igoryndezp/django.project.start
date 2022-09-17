e396d3a72cfc51aeb65752402da239e5016ca56d
igoryndezp <igoryndezp@gmail.com>
2022-09-17 22:35:47 +0300

    New.seting.admin

--- testproject/blog/admin.py
+++ testproject/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id",'title','photo')
 
-
-admin.site.register(Article)
-
+admin.site.register(Article, ArticleAdmin)
